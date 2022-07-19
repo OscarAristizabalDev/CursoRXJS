2e107ab0d2523e9853bb65a6b3e9f0b4ae20755b
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-07-19 16:32:20 -0500

    Primer observable, utilizando metodo next y complete

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-
-
-console.log('Hola Mundo!');
-
+import { Observable } from 'rxjs';
 
 
 
+//const observable$ = Observable.create();
+const observable$ = new Observable<string>( subscriber => {
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
 
+    subscriber.complete();
 
+    subscriber.next('Hola 2');
+    subscriber.next('Mundo 2');
+});
 
+observable$.subscribe(resp => console.log(resp) )
