4b59c067a8dbe855a78c1ca98e5bba9bd82bcc2c
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-07-19 16:50:36 -0500

    Se organiza el codigo en carpetas para manejar una biblioteca de lo que se va aprendiendo

---
+++ src/Observables/01-observable.ts
@@ -0,0 +1,32 @@
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<any> = {
+    next: valor => console.log('siguiente  [next]: ', valor),
+    error: error => console.warn('error [obs]: ', error),
+    complete: () => console.info('Completado [obs]: ')
+}
+
+//const observable$ = Observable.create();
+const observable$ = new Observable<string>( subscriber => {
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
+
+    // forzar un error
+    // const a = undefined;
+    // a.nombre = 'Fernando';
+
+    subscriber.complete();
+
+    subscriber.next('Hola 2');
+    subscriber.next('Mundo 2');
+});
+
+// Forma 1
+observable$.subscribe(observer);
+
+// Forma 2
+observable$.subscribe({
+    next: valor => console.log('next: ', valor),
+    error: error => console.warn('error: ', error),
+    complete: () => console.log('Completado')
+});
--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,4 @@ const observer: Observer<any> = {
     complete: () => console.info('Completado [obs]: ')
 }
 
-//const observable$ = Observable.create();
-const observable$ = new Observable<string>( subscriber => {
-    subscriber.next('Hola');
-    subscriber.next('Mundo');
-
-    // forzar un error
-    // const a = undefined;
-    // a.nombre = 'Fernando';
-
-    subscriber.complete();
-
-    subscriber.next('Hola 2');
-    subscriber.next('Mundo 2');
-});
-
-// Forma 1
-observable$.subscribe(observer);
-
-// Forma 2
-observable$.subscribe({
-    next: valor => console.log('next: ', valor),
-    error: error => console.warn('error: ', error),
-    complete: () => console.log('Completado')
-});
-
     
