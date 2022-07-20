660c8c31a3e1bff139e11f03b59cf875f90cf57a
OscarAristizabalDev <oaristizabal@dataifx.com>
2022-07-20 10:25:59 -0500

    Se utiliza el subscribe y unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,24 @@ const observer: Observer<any> = {
     complete: () => console.info('Completado [obs]: ')
 }
 
-    
+const intervalo$ = new Observable<number>(subscriber => {
+    // crear un contador, 1,2..., 3
+    let contador = 0;
+    const interval = setInterval(() => {
+        contador++;
+        subscriber.next(contador);
+    }, 1000);
+
+    return () => {
+        clearInterval(interval);
+        console.log('Intervalo destruido');
+    }
+});
+
+const subscription = intervalo$.subscribe(num => console.log('Num: ', num));
+
+setTimeout(() => {
+    subscription.unsubscribe();
+
+    console.log('Completado timeout');
+}, 3000);
